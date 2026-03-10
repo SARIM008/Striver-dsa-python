67f0d69f4f01fdf2357f0e8443e7b885d75ec23c
SARIM008 <samaltic008@gmail.com>
2026-03-11 04:24:26 +0530

    another way of name recursion

--- basic recursion/01 name_print_n_times.py
+++ basic recursion/01 name_print_n_times.py
@@ -6,3 +6,11 @@ def name(n,name):
         helper(i+1)
     helper(1)
 name(5,"sarim")
+
+def name2(i,n,name):
+    if i>n:
+        return
+    i+=1
+    print(name)
+    name2(i,n,name)
+name2(1,7,"yasir")
