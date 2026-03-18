0177115953a323f32acaa27755e82d5279a8e8f2
SARIM008 <samaltic008@gmail.com>
2026-03-18 22:35:05 +0530

    sorting

---
+++ sorting/01_selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index=i
+        for j in range(i+1,len(arr)):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    print(arr)
+selection_sort([10,100,20,25,3,96,5,4])
+
+
---
+++ sorting/02_bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_Sort(arr):
+    for i in range(len(arr)):
+        swap=False
+        for j in range(0,len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swap=True
+        
+        if not swap:
+            break
+    print(arr)
+bubble_Sort([10,100,20,25,3,96,5,4])
